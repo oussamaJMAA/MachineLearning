2cf0ed69593f669309b07c34dd52d066f8d57089
oussama jmaa <jemaaoussama64@gmail.com>
2022-08-30 07:08:59 +0100

    backward elimination

--- MultipleLinearRegression/multiple_linear_regression.py
+++ MultipleLinearRegression/multiple_linear_regression.py
@@ -29,4 +29,22 @@ lm.fit(X_train, y_train)
 
 y_pred = lm.predict(X_test)
 
-print(lm.coef_)
+# Backword elimination :
+import statsmodels.api as sm
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]].tolist()
+regressor_ols = sm.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+X_opt = X[:, [0, 1, 3, 4, 5]].tolist()
+regressor_ols = sm.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+X_opt = X[:, [0, 3, 4, 5]].tolist()
+regressor_ols = sm.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+X_opt = X[:, [0, 3, 5]].tolist()
+regressor_ols = sm.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
+X_opt = X[:, [0, 3]].tolist()
+regressor_ols = sm.OLS(endog=y, exog=X_opt).fit()
+print(regressor_ols.summary())
